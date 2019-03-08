fca527476c130b0d07cc2badfb09f46c289944b9
mirifi2k <46975339+mirifi2k@users.noreply.github.com>
2019-03-08 15:30:13 +0200

    Add files via upload

---
+++ client.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+char *ip;
+int port;
+int sockfd;
+
+struct sockaddr_in server_address;
+
+void read_params(char *path)
+{
+    FILE *f = fopen(path, "r");
+    ip = (char *) malloc(20);
+
+    char *s_port = (char *)malloc(20);
+    fscanf(f, "ip=%s\nport=%s", ip, s_port);
+
+    if (f)
+        fclose(f);
+    
+    port = atoi(s_port);
+    printf("%s %d\n", ip, port);
+}
+
+void sendMessage()
+{
+	char buff[1024];
+	sprintf(buff, "test message.");
+
+	write(sockfd, buff, strlen(buff));
+	buff[0] = 0;
+
+	int r;
+	r = read(sockfd, buff, sizeof(buff));
+	buff[r] = 0;
+
+	if (r)
+		fprintf(stdout, "%s\n", buff);
+}
+
+void clientSetup()
+{
+	if ((sockfd = socket(PF_INET, SOCK_STREAM, 0)) < 0)
+	{
+		exit(EXIT_FAILURE);
+	}
+
+	server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = inet_addr(ip);
+    server_address.sin_port = htons(port);
+
+    if (connect(sockfd, (struct sockaddr *) &server_address, sizeof(server_address)) < 0)
+    {
+    	exit(EXIT_FAILURE);
+    }
+
+    sendMessage();
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+    {
+        printf("Usage %s <config_file_path>\n", argv[0]);
+        exit(1);
+    }    
+    read_params(argv[1]);
+    clientSetup();
+
+    return 0;
+}
--- server.c
+++ server.c
@@ -1,26 +1,88 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
+#include <signal.h>
 
-int ip, port;
+char *ip;
+int port;
+
+struct sockaddr_in server_address, remote_address;
+int sockfd;
+socklen_t len;
 
 void read_params(char *path)
 {
     FILE *f = fopen(path, "r");
-    char *s_ip = (char *)malloc(20), *s_port = (char *)malloc(20);
-    fscanf(f, "ip=%s\nport=%s", s_ip, s_port);
+    ip = (char *) malloc(20);
+
+    char *s_port = (char *)malloc(20);
+    fscanf(f, "ip=%s\nport=%s", ip, s_port);
+
     if (f)
         fclose(f);
-    ip = atoi(s_ip);
+    
     port = atoi(s_port);
-    printf("%d %d\n", ip, port);
+    printf("%s %d\n", ip, port);
 }
 
-void server() {
+void handleRequest(int connfd)
+{
+    char buff[1024];
+    int r;
+
+    r = read(connfd, buff, sizeof(buff));
     
+    buff[r] = '1';
+    buff[r + 1] = 0;
+
+    write(connfd, buff, r + 1);
+    close(connfd);
+    exit(0);
+}
+
+void acc()
+{
+    printf("works\n");
+
+    len = sizeof(remote_address);
+    int connfd = accept(sockfd, (struct sockaddr *) &remote_address, &len);
+    pid_t pid;
+
+    if ((pid = fork()) == 0) {
+        printf("pid %d\n", getpid());
+        handleRequest(connfd);
+    }
+
+    else
+    {
+        acc();
+    }
+}
+
+void serverSetup() 
+{
+    if ((sockfd = socket(PF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        exit(EXIT_FAILURE);
+    }
+
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = inet_addr(ip);
+    server_address.sin_port = htons(port);
+
+    if (bind(sockfd, (struct sockaddr *) &server_address, sizeof(server_address)) < 0)
+    {
+        printf("binding failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    listen(sockfd, 5);
+    acc();
 }
 
 int main(int argc, char *args[])
@@ -29,7 +91,12 @@ int main(int argc, char *args[])
     {
         printf("Usage %s <config_file_path> <root>.\n", args[0]);
         exit(1);
-    }
+    }   
+
+    signal(SIGCHLD, SIG_IGN);
+
     read_params(args[1]);
+    serverSetup();
+
     return 0;
 }
